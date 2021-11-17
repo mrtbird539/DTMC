02a0f8f6972825251f219ea889d7411c19d9ba2a
OneFJef <jef@project87.net>
2021-11-17 12:02:32 -0500

    Added  modifications to car schema.

--- server/models/Car.js
+++ server/models/Car.js
@@ -4,15 +4,34 @@ const carSchema = new Schema({
   year: {
     type: Number,
     required: true,
+    min: 1885,
   },
   make: {
     type: String,
     required: true,
+    trim: true,
+    lowercase: true,
   },
   model: {
     type: String,
     required: true,
+    trim: true,
+    lowercase: true,
   },
+  modification: [
+    {
+      preformance: {
+        type: String,
+      },
+      functional: {
+        type: String,
+      },
+      cosmetic: {
+        type: String,
+      },
+    },
+  ],
+  // Color?
 });
 
 const Car = model("Car", carSchema);
