c5190d9e9dbef58330d8e775429c22a02e108763
OneFJef <jef@project87.net>
2021-11-17 12:19:07 -0500

    Created typeDefs objects and started on tyepDefs queries.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,27 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type User {
+        _id: ID
+        userName: String
+        email: String
+        cars: [Car]
+    }
+
+    type Car {
+        _id: ID
+        year: Number
+        make: String
+        model: String
+        modification: [preformance, functional, cosmetic]
+        user: User
+    }
+
+    type Query {
+        me: User
+        user(id: ID!): User
+        
+    }
+`;
 
 module.exports = typeDefs;
