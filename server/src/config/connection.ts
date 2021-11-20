275d6653fc9bd99553ea2643d15a922648aff93e
Tyler Smith <mrtbird539@gmail.com>
2021-11-20 10:32:51 -0500

    Updated models

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 import * as dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || process.env.DB_URI!);
+mongoose.connect(process.env.DB_URI!);
 
 export default mongoose.connection;
