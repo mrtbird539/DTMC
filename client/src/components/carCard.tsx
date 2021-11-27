cf3d5bb98fb791ab2ab4813ed6288652b1ee6e48
Jas <ragnarock.josef@gmail.com>
2021-11-27 12:29:33 -0500

    cleaned up class names

--- client/src/components/carCard.tsx
+++ client/src/components/carCard.tsx
@@ -4,28 +4,26 @@ export const CarCard = (props: any) => {
 
     return (
         <div className="has-background-black column is-4 is-offset-1">
-            <div className="card is-fullwidth">
-                <header className="card-back-color card-header">
+            <div className="card card-back-color is-fullwidth">
+                <header className="card-header">
                     <p className="has-text-white card-header-title">
                         {props.title}
                     </p>
                 </header>
-                <div className="card-back-color card-image">
+                <div className="card-image">
                     <figure className="image">
-                        {/* <!-- Image is a link to the live site --> */}
-                        <a href= {props.userCarPhoto} target="_blank" rel="noopener noreferrer">
-                            <img src= {props.userCarPhoto}
+                        <a href={props.userCarPhoto} target="_blank" rel="noopener noreferrer">
+                            <img src={props.userCarPhoto}
                                 alt="CarPhoto"></img>
                         </a>
                     </figure>
                 </div>
-                <div className="card-back-color content">
+                <div className="content">
                     <p className="has-text-white title is-4">Modifications:</p>
                     <p className="has-text-white subtitle is-6">{props.modList}</p>
-
                 </div>
-                <footer className="card-back-color card-footer">
-                    {/* <!-- Skills & technologies used in application --> */}
+                {props.editCarButton}
+                <footer className="card-footer">
                     <p className="has-text-white subtitle is-small">{props.userInfo}</p>
                 </footer>
             </div>
