3ed7181961d133c29d86ef9d250ccf15e1a8289b
kelley-mcd <kmcdonnell8@gmail.com>
2021-11-27 10:03:34 -0500

    added css file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import 'bulma/css/bulma.min.css';
+import './components.css';
 import './App.css';
 
 import { Header } from './components/header';
