88e567cb73c3b113d1946427900c8d31a09ab74c
kelley-mcd <kmcdonnell8@gmail.com>
2021-11-29 18:41:24 -0500

    font change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import 'bulma/css/bulma.min.css';
+import 'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Shippori+Antique+B1&display=swap';
 import './components.css';
 import './App.css';
 
