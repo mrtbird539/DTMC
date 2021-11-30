ab4fdbdde7b8faeb5fa354db9d48f4b9b591ad42
Alex Parker <alex.n.parker82@gmail.com>
2021-11-29 21:55:03 -0500

    creating user

--- client/src/components/userProfile.tsx
+++ client/src/components/userProfile.tsx
@@ -1,27 +1,23 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0, User } from "@auth0/auth0-react";
 import { useMutation } from "@apollo/client";
 import { CREATE_USER } from "../utils/mutations";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading }: any = useAuth0();
-  // const [userCreate, { error, data }] = useMutation(CREATE_USER);
+  const [userCreate, { error, data }] = useMutation(CREATE_USER);
+  const { user, isAuthenticated, isLoading }: User = useAuth0();
+  console.log(user);
 
-  // const handleUserCreate = async () => {
-  //       console.log(user.username);
-  //       const { data } = await userCreate({
-  //         variables: {
-  //           userName: user.name,
-  //           email: user.email
-  //         }
-  //       });
-  // };
-
-  // if (isAuthenticated) {
-  //   handleUserCreate();
-  //   return;
-  // };
-
+  const handleUserCreate = async () => {
+        console.log(user.email);
+        const { email, name, username } = user;
+        const { data } = await userCreate({
+          variables: {
+            userName: username || name,
+            email
+          }
+        });
+  };
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -32,7 +28,8 @@ const Profile = () => {
       <div>
         <div className="box user-comp-box">
           <img className="is-rounded" src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
+          <h2>{user.username}</h2>
+          <button onClick={() => handleUserCreate()}>Add User</button>
 
         </div>
       </div>
