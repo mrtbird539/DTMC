d6cb0789fd06d93790ddbe6cd1c30d570c3b6af8
Alex Parker <alex.n.parker82@gmail.com>
2021-11-29 20:56:04 -0500

    almost got DB user create to work

--- client/src/components/userProfile.tsx
+++ client/src/components/userProfile.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation } from "@apollo/client";
+import { CREATE_USER } from "../utils/mutations";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading }: any = useAuth0();
+  // const [userCreate, { error, data }] = useMutation(CREATE_USER);
+
+  // const handleUserCreate = async () => {
+  //       console.log(user.username);
+  //       const { data } = await userCreate({
+  //         variables: {
+  //           userName: user.name,
+  //           email: user.email
+  //         }
+  //       });
+  // };
+
+  // if (isAuthenticated) {
+  //   handleUserCreate();
+  //   return;
+  // };
+
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -14,7 +33,7 @@ const Profile = () => {
         <div className="box user-comp-box">
           <img className="is-rounded" src={user.picture} alt={user.name} />
           <h2>{user.name}</h2>
-          
+
         </div>
       </div>
     )
