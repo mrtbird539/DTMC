b136be9c750b74955356112416bfb3399c751b41
Jas <ragnarock.josef@gmail.com>
2021-12-01 18:23:44 -0500

    Changed mod layout

--- client/src/components/carCard.tsx
+++ client/src/components/carCard.tsx
@@ -4,10 +4,11 @@ export const CarCard = (props: any) => {
 
     return (
         <>
+        <div className="column is-1"></div>
             <div className="car-border has-background-black column is-4">
-                <div className="card is-fullwidth card-back-color">
+                <div className="card is-fullwidth card-back-color is-offset-1">
                     <header className="card-back-color card-header">
-                        <p className="has-text-white card-header-title">
+                        <p className="title is-3">
                             {props.title}
                         </p>
                     </header>
@@ -20,8 +21,16 @@ export const CarCard = (props: any) => {
                         </figure>
                     </div>
                     <div className="content">
-                        <p className="has-text-white title is-4">Modifications:</p>
-                        <p className="has-text-white subtitle is-6">{props.modList}</p>
+                        <p className="title is-4">Performance Mods</p>
+                        <p className="has-text-white subtitle is-6">{props.modPerformance}</p>
+                    </div>
+                    <div className="content">
+                        <p className="title is-4">Functional Mods</p>
+                        <p className="has-text-white subtitle is-6">{props.modFunctional}</p>
+                    </div>
+                    <div className="content">
+                        <p className="title is-4">Cosmetic Mods</p>
+                        <p className="has-text-white subtitle is-6">{props.modCosmetic}</p>
                     </div>
                     <footer className="card-footer">
                         <p className="has-text-white subtitle is-small">{props.userInfo}</p>
