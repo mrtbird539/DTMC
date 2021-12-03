44f895d9fc66472d9d9dafb93989242620c376aa
Jas <ragnarock.josef@gmail.com>
2021-12-02 21:18:13 -0500

    changed id on elements

--- client/src/components/editCard.tsx
+++ client/src/components/editCard.tsx
@@ -11,6 +11,14 @@ export const EditCard = (props: any) => {
         return value;
     };
 
+    // const saveBtn = (props: any) => {
+    //     return<button onClick={props.saveBtn} className="button is-link" id="submit" type="button">Save</button>
+    // };
+
+    // const cancelBtn = (props: any) => {
+    //     return <button onClick={props.canelBtn} className="button is-danger" id="submit" type="button">Cancel</button>
+    // };
+
 
     return (
         <div className="has-background-black column is-6 is-offset-3">
@@ -26,7 +34,7 @@ export const EditCard = (props: any) => {
                             <div className="card-back-color field">
                                 <label className="label">Make, Model, Year</label>
                                 <div className="control">
-                                    <DropDown />
+                                    <DropDown className="dropdown" id="dropdown" name="dropdown"/>
                                 </div>
                             </div>
 
@@ -40,26 +48,26 @@ export const EditCard = (props: any) => {
                             <div className="field">
                                 <label className="label">Photo:</label>
                                 <div className="control">
-                                    <textarea className="input" id="description" placeholder="Enter URL Here" name="description"></textarea>
+                                    <textarea className="input" id="photo" placeholder="Enter URL Here" name="photo"></textarea>
                                 </div>
                             </div>
 
                             <div className="card-back-color field">
                                 <div className="has-text-white control">
                                     <label className="label">Performance Mods</label>
-                                    <textarea className="input" id="description" placeholder={props.modPerformance} name="description"></textarea>
+                                    <textarea className="input" id="performance-mods" placeholder={props.modPerformance} name="performance-mods"></textarea>
                                 </div>
                             </div>
                             <div className="card-back-color field">
                                 <div className="has-text-white control">
                                     <label className="label">Functional Mods</label>
-                                    <textarea className="input" id="description" placeholder={props.modFunctional} name="description"></textarea>
+                                    <textarea className="input" id="functional-mods" placeholder={props.modFunctional} name="functional-mods"></textarea>
                                 </div>
                             </div>
                             <div className="card-back-color field">
                                 <div className="has-text-white control">
                                     <label className="label">Cosmetic Mods</label>
-                                    <textarea className="input" id="description" placeholder={props.modCosmetic} name="description"></textarea>
+                                    <textarea className="input" id="cosmetic-mods" placeholder={props.modCosmetic} name="cosmetic-mods"></textarea>
                                 </div>
                             </div>
                             {/* 
@@ -71,6 +79,8 @@ export const EditCard = (props: any) => {
                         </div> */}
 
                             <div className="field is-grouped">
+                                {/* {saveBtn}
+                                {cancelBtn} */}
                                 <div className="control">
                                 </div>
                             </div>
